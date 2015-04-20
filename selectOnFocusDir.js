1fbddd2526978fc8cba22c97b5fc055d116e95af
SteveL82 <steve.lievens@pictoonline.be>
2015-04-20 11:48:14 +0200

    v1.0.0

---
+++ selectOnFocusDir.js
@@ -0,0 +1,18 @@
+﻿angular.module('ui').directive('selectOnFocus', function () {
+	// Linker function
+	return function (scope, element, attrs) {
+		element.on('click', function (e) {
+			var $this = $(this);
+			if ($this.hasClass('focus'))
+				return;
+
+			$this.addClass('focus');
+			this.select();
+		});
+
+		element.on('blur', function (e) {
+			var $this = $(this);
+			$this.removeClass('focus');
+		});
+	};
+});
